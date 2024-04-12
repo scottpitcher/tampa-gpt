bcac32cd66bad2e4901791d131f17aa07f23f819
scottpitcher <85075186+scottpitcher@users.noreply.github.com>
2024-04-12 11:44:05 -0400

    commit

--- webscraping.py
+++ webscraping.py
@@ -8,13 +8,17 @@ def get_article_text(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    paragraphs = soup.find('article').find('section').find_all('p')
+    paragraphs = soup.find_all('p')
 
     # Iterate through each paragraph, getting the text and appending to the list
     paragraph_texts = [paragraph.get_text() for paragraph in paragraphs]
     
     return paragraph_texts
 
-print(get_article_text(url))
-    
 
+data = get_article_text(url)
+
+with open("data.txt", 'w') as outfile:
+    for line in data:
+        outfile.write(line)
+    
