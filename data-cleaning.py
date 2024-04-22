835389487ad78fb32cb3c6e3e14dc2c81a7101ef
scottpitcher <85075186+scottpitcher@users.noreply.github.com>
2024-04-22 16:39:21 -0400

    Updated data-cleaning.py

--- data-cleaning.py
+++ data-cleaning.py
@@ -33,7 +33,7 @@ def text_processing(line):
         Tampa has beautiful sunsets.
         Tampa has a lively cultural scene.
         The best time to visit Tampa is during spring.       
-        Input paragraph: 'New York City, a city within New York State, is the most populated in the country. Even though the city is expensive to live in, there are many things to do there!'
+        Input paragraph: 'New York City, a city within New York State, is the most populated city in the country. Even though the city is expensive to live in, there are many things to do there!'
         Output points:
         New York City is a city within New York State.
         NYC is the most populated city in the United States.
