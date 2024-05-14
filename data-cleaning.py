714409bd2428f80d215f698ef5c3d12b428f1263
scottpitcher <85075186+scottpitcher@users.noreply.github.com>
2024-05-14 15:36:27 -0400

    Tuned User and System prompts

--- data-cleaning.py
+++ data-cleaning.py
@@ -14,33 +14,51 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-messages = [{"role": "system",
-        "content": """You are a helpful assistant whose goal is to break up paragraphs into separate lines with independent information,
-             ensuring each line can be read without needing context from the input or other lines. Each line should start with a clear subject 
-             and avoid using pronouns that refer to previously mentioned subjects."""},        
+message_template = [
+        # Initial system prompt
+        {"role": "system",
+        "content": """You are a helpful assistant whose goal is to read in the user's paragraph and break the paragraph into separate lines of independent information,
+             ensuring each line can be read without context from the input or other lines. Each line should start with a clear subject 
+             and avoid using pronouns that refer to other lines."""
+             },
+           
+        # Example user input 1
         {"role": "user", 
-        
-        "content": f"""The following are examples of paragraphs broken into separate lines:
-        Input paragraph: 'Tampa is famous for its vibrant waterfront parks, beautiful sunsets, and lively cultural scene. The best time to visit is during spring.'
-        Output points:
-        Tampa is famous for vibrant waterfront parks.
+        "content": f"Tampa is famous for its vibrant waterfront parks, beautiful sunsets, and lively cultural scene. The best time to visit is during spring."
+        },
+        # Example assistant output 1
+        {"role": "assistant",
+        "content":
+        """Tampa is famous for vibrant waterfront parks.
         Tampa has beautiful sunsets.
         Tampa has a lively cultural scene.
-        The best time to visit Tampa is during spring.       
-        Input paragraph: 'New York City, a city within New York State, is the most populated city in the country. Even though the city is expensive to live in, there are many things to do there!'
-        Output points:
-        New York City is a city within New York State.
+        The best time to visit Tampa is during spring.
+        """
+        },
+        # Example user input 2
+        {"role": "user", 
+        "content": f"New York City, a city within New York State, is the most populated city in the country. Even though the city is expensive to live in, there are many things to do there!"
+        },
+        # Example assistant output2
+        {"role": "assistant",
+        "content":"""New York City is a city within New York State.
         NYC is the most populated city in the United States.
         NYC is expensive to live in.
-        Despite the high cost of living, there are many things to do in NYC!"""
-        }]
+        Despite the high cost of living, there are many things to do in NYC!
+        """},
+        {"role": "user", 
+        "content": ""}]
 
 def text_processing(line):
     """Creating a function to read in the file and break up paragraphs with a pretrained model"""
+    messages=message_template.copy()
+    messages[5]["content"] = line
+
     chat_completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=messages
-)
+    )
+
     new_line = chat_completion.choices[0].message.content
     return new_line
 
@@ -73,7 +91,7 @@ def break_up_text(text):
         # Error warning if neither supported data type is inputted
         return ("Unsupported data type")
 
-break_up_text("The city is part of the Tampa-St. Petersburg-Clearwater, Florida Metropolitan Statistical Area, which is a four-county area composed of roughly 3.1 million residents,[14] making it the second-largest metropolitan statistical area (MSA) in the state and the sixth largest in the Southeastern United States, behind Dallas-Fort Worth, Houston, Washington D.C., Atlanta, and Miami.[15] The Greater Tampa Bay area has over 4 million residents and generally includes the Tampa and Sarasota metro areas.")
+break_up_text("Tampa is part of the Tampa-St. Petersburg-Clearwater, Florida Metropolitan Statistical Area, which is a four-county area composed of roughly 3.1 million residents,[14] making it the second-largest metropolitan statistical area (MSA) in the state and the sixth largest in the Southeastern United States, behind Dallas-Fort Worth, Houston, Washington D.C., Atlanta, and Miami.[15] The Greater Tampa Bay area has over 4 million residents and generally includes the Tampa and Sarasota metro areas.")
 
 # Initialising paginator for higher file volumes in bucket
 paginator = s3.get_paginator("list_objects_v2")
